7aa071a10d62d1f9c1fd8a8cd1816514368ea1cb
keyframes <i@zgso.me>
2017-08-12 12:15:00 +0800

    提交

--- 1.py
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env python3
-# _*_ coding:utf-8 _*_
-print(1)
---
+++ list.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# _*_ coding:utf-8 _*_
+
+L = [1,2,3,True]
+
+# 获取列表中的元素
+print( L[0] )
+print( L[len(L)-1] )
+print( L[-1] )
+
+# 追加元素
+
+
+
+
+
+
